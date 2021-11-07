58eb7f6c854c4685e55e6650bd4ed32faaca7eb1
yomusfu <73890282+yomusfu@users.noreply.github.com>
2021-11-07 19:50:06 +0300

    Task2

--- src/Task17/Tasks.java
+++ src/Task17/Tasks.java
@@ -19,28 +19,6 @@ public class Tasks {
         return ans;
     }
 
-    public static boolean hasDigitPlus(String s){
-        return Pattern.compile("\\d+\\s*\\+").matcher(s).find();
-    }
-
-    public static boolean isDate(String s){
-        Matcher m = Pattern.compile(
-                "((29/02/((19([2468][048])|([13579][26])|(0[48]))|([2-9]\\d([2468][048])|([13579][26]))|([2468][048]00)|([3579][26]00)))|((2[0-8]/02/((19\\d\\d)|([2-9]\\d{3}))))|(((0?\\d)|([12]\\d)|(3[01]))/((0?[013456789])|(1[12]))/((19\\d\\d)|([2-9]\\d{3}))))"
-        ).matcher(s);
-        return m.find() && m.group().equals(s);
-    }
-
-    public static boolean checkEmail(String s){
-        Matcher m = Pattern.compile(
-                "[a-zA-Z0-9]+[a-zA-Z0-9!#$%&'*+\\-/=?^_`{|}~.]*@[a-zA-Z0-9\\-_.]+\\.[a-zA-Z]+"
-                //"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])"
-        ).matcher(s);
-        return m.find() && m.group().equals(s);
-    }
-    public static boolean checkPass(String s){
-        return Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z_]{8,}").matcher(s).find();
-    }
-
     public static void main(String[] args) {
         //ex2
         System.out.println(isThatStr("abcdefghijklmnopqrstuv18340"));
