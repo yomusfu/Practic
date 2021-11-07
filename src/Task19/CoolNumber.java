512d0c16b21083c35dbedccdff9258f38f41fce2
yomusfu <73890282+yomusfu@users.noreply.github.com>
2021-11-07 21:32:36 +0300

    Task2

--- src/Task19/CoolNumber.java
+++ src/Task19/CoolNumber.java
@@ -1,6 +1,64 @@
 package Task19;
 
+import jdk.incubator.foreign.LibraryLookup;
+
+import java.util.ArrayList.*;
+import java.lang.Enum.*;
+import java.util.HashSet.*;
+import java.util.TreeSet.*;
+import java.util.Scanner.*;
+
 public class CoolNumber {
-    char x,y,z;
-    int n,r;
+    Symbols x,y,z;
+    double n,r;
+
+    public CoolNumber() {
+        this.n = 0;
+        this.r = 0;
+    }
+
+    public CoolNumber(Symbols x, Symbols y, Symbols z, double n, double r) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.n = n;
+        this.r = r;
+    }
+
+    @Override
+    public String toString() {
+        return "CoolNumber{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                ", n=" + n +
+                ", r=" + r +
+                '}';
+    }
+
+    public String Generator(){
+
+        String Nomer="";
+
+        n=(Math.random()*((9-0)+1))+0;
+        r=(Math.random()*((199-1)+1))+1;
+
+        Symbols[] symbolOption = Symbols.values();
+
+        x=symbolOption[(int) (Math.random()*(((12-1)+1))+1)];
+        y=symbolOption[(int) (Math.random()*(((12-1)+1))+1)];
+        z=symbolOption[(int) (Math.random()*(((12-1)+1))+1)];
+
+        return Nomer;
+    }
+    public void OutPut (Symbols x, Symbols y, Symbols z, double n, double r){
+
+
+        System.out.print(n);
+        System.out.print(n);
+        System.out.print(n);
+
+
+        System.out.print(r);
+    }
 }
---
+++ src/Task19/Symbols.java
@@ -0,0 +1,16 @@
+package Task19;
+
+public enum Symbols {
+    A,
+    B,
+    E,
+    K,
+    M,
+    H,
+    O,
+    P,
+    C,
+    T,
+    Y,
+    X,
+}
